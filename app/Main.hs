1d1becd31916b92fadf647971cf3506218babb0c
WiktorB2004 - Ubuntu <byrka.w@gmail.com>
2023-11-03 21:28:42 +0100

    Add TODO(#5)

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,7 @@
 -- TODO(#2): Implement functions to parse user input into accessible format
 -- TODO(#3): Implement basic mathematical operations (+,-,/,*)
 -- TODO(#4): Implement more advanced mathematical operations (**/^, roots etc.)
--- TODO: Implement solving linear and quadratical equations
+-- TODO(#5): Implement solving linear and quadratical equations
 -- TODO: (Optional) Add scientific notation handling
 -- TODO: (Optional) Add decimal to binary translation and operations on binary numbers
 

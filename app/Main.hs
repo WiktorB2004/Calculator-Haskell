36551c9c690da4b8fc92e36093777328bb5db26a
WiktorB2004 - Ubuntu <byrka.w@gmail.com>
2023-11-03 21:28:38 +0100

    Add TODO(#1)

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,11 @@
+-- TODO(#1): Implement functions to get user input and print the result
+-- TODO: Implement functions to parse user input into accessible format
+-- TODO: Implement basic mathematical operations (+,-,/,*)
+-- TODO: Implement more advanced mathematical operations (**/^, roots etc.)
+-- TODO: Implement solving linear and quadratical equations
+-- TODO: (Optional) Add scientific notation handling
+-- TODO: (Optional) Add decimal to binary translation and operations on binary numbers
+
 module Main (main) where
 
 import Lib

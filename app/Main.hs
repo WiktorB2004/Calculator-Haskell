918357e90dafc89ce8590572937621d261f49dab
WiktorB2004 - Ubuntu <byrka.w@gmail.com>
2023-11-03 21:40:36 +0100

    Renamed toto and added new one

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,10 @@
 -- TODO(#2): Implement functions to parse user input into accessible format
 -- TODO(#3): Implement basic mathematical operations (+,-,/,*)
 -- TODO(#4): Implement more advanced mathematical operations (**/^, roots etc.)
--- TODO(#5): Implement solving linear and quadratical equations
+-- TODO(#5): Implement solving linear and quadratic equations
 -- TODO(#6): (Optional) Add scientific notation handling
 -- TODO(#7): (Optional) Add decimal to binary translation and operations on binary numbers
+-- TODO Create README.md file
 
 module Main (main) where
 

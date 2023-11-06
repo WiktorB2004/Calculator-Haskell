338a05caad7a069ab417a8203cea2db967705c9c
WiktorB2004 - Ubuntu <byrka.w@gmail.com>
2023-11-06 22:20:34 +0100

    Solved RPN Conversion and its todos - handling of negative numbers and parentheses Fixes #2

--- src/Calculator.hs
+++ src/Calculator.hs
@@ -1,7 +1,7 @@
 module Calculator (getResult) where
 
 import Data.Char (isDigit)
-import Stack (Stack (..), empty, isEmpty, pop, push, top)
+import Stack (Stack (..), empty, isEmpty, pop, push)
 
 data ExpElem
   = Operand String
@@ -12,11 +12,11 @@ instance Show ExpElem where
   show (Sign p) = show p
 
 isOperand :: String -> Bool
-isOperand chr =
-  fromEnum (head chr) > 64 && fromEnum (head chr) < 124 || isDigit (head chr)
+isOperand str =
+  fromEnum (length str) == 1 && fromEnum (head str) > 64 && fromEnum (head str) < 124 || all isDigit (tail str) && isOperator [head str] || all isDigit str
 
 isOperator :: String -> Bool
-isOperator chr = head chr `elem` "=+-*/"
+isOperator = all (`elem` "=+-*/")
 
 -- Convert infix expression to RPN
 infixToRPN :: String -> [ExpElem]
@@ -28,22 +28,27 @@ processTokens [] stack output = output ++ pushOperators stack
 processTokens (token : tokens) stack output
   | isOperand token = processTokens tokens stack (output ++ [Operand token])
   | token == "(" = processTokens tokens (push token stack) output
-  | token == ")" = let (out, newStack) = handleClosingBracket stack in processTokens tokens newStack (output ++ out)
-  | isOperator token = let (out, newStack) = handleOperator token stack in processTokens tokens newStack (output ++ out)
-  | otherwise = error "Incorrect expression provided"
+  | token == ")" = let (out, newStack) = handleClosingBracket stack [] in processTokens tokens newStack (output ++ out)
+  | isOperator token = let (out, newStack) = handleOperator token stack [] in processTokens tokens newStack (output ++ out)
+  | otherwise = output
 
-handleClosingBracket :: Stack String -> ([ExpElem], Stack String)
-handleClosingBracket _ = ([], empty)
-
-handleOperator :: String -> Stack String -> ([ExpElem], Stack String)
-handleOperator token stack
+handleClosingBracket :: Stack String -> [ExpElem] -> ([ExpElem], Stack String)
+handleClosingBracket stack output
   | not (isEmpty stack) =
       let (stackTop, newStack) = pop stack
-       in if precendence stackTop <= precendence token
-            then ([Sign (head stackTop)], newStack)
-            else ([], push token stack)
-  | isEmpty stack = ([], push token stack)
-  | otherwise = ([], stack)
+       in if stackTop == "("
+            then (output, newStack)
+            else handleClosingBracket newStack (output ++ [Sign (head stackTop)])
+  | otherwise = error "Unbalanced parenthesis"
+
+handleOperator :: String -> Stack String -> [ExpElem] -> ([ExpElem], Stack String)
+handleOperator token stack output
+  | not (isEmpty stack) =
+      let (stackTop, newStack) = pop stack
+       in if precendence stackTop >= precendence token
+            then handleOperator token newStack (output ++ [Sign (head stackTop)])
+            else (output, push token stack)
+  | otherwise = (output, push token stack)
 
 pushOperators :: Stack String -> [ExpElem]
 pushOperators stack

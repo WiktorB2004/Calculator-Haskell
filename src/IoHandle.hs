0d04eaf2767ebf0e2f609e16e4c9b3a0b6b5b278
WiktorB2004 - Ubuntu <byrka.w@gmail.com>
2023-11-11 13:52:32 +0100

    Created option to choose mode of calculator

--- src/IoHandle.hs
+++ src/IoHandle.hs
@@ -5,7 +5,15 @@ import Calculator
 -- Manages input and output of program.
 inputOutput :: IO ()
 inputOutput = do
-  putStrLn "Enter the operation or equation:"
+  putStrLn "Enter the action to take:"
+  putStrLn "1. Operation"
+  putStrLn "2. Linear equation"
+  putStrLn "3. Quadratic equation"
+  mode <- getLine
+  case mode of
+    "1" -> putStrLn "Enter the operation: "
+    "2" -> putStrLn "Enter the linear equation constants: a b "
+    "3" -> putStrLn "Enter the quadratic equation constants: a b c"
+    _ -> error "Incorrect mode selected"
   str <- getLine
-  print (getResult str)
-  putStrLn ""
+  print (getResult str mode)
